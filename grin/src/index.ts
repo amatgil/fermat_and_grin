a0f69f85f2245dad3beb8a68f8fcabada4baface
Gerard Félez Guerrero <gerard.felez@estudiantat.upc.edu>
2026-05-21 17:54:35 +0200

    Creates a function to compute the GRIN refraction indicies

--- grin/src/index.ts
+++ grin/src/index.ts
@@ -5,12 +5,60 @@ export class ExperimentGrin {
   g: CanvasRenderingContext2D;
   g_width: number;
   g_height: number;
+  n1: number;
+  delta: number;
+  num_regions: number;
+  
+  angle_raig: number;
+  radi_fibra: number;
+  alpha: number;
+
+  longitud_cable: number;
+  ns: number[];
+
+
 
   constructor(g: CanvasRenderingContext2D, width: number, height: number) {
     this.g = g;
     this.g_width = width;
     this.g_height = height;
+
+    this.n1 = 1.47;
+    this.delta = 0.01;
+    this.num_regions = 10;
+    this.angle_raig = 1; // En radians
+    this.radi_fibra = 1; // Ni idea de quines unitats posar lmao
+    this.alpha = 1;
+    this.longitud_cable = 100; // En metres (suposo)
+    this.ns = [];
   }
+
+  /**
+   * Computes the index of refraction in a GRIN fiber optic cable using the following formula:
+   * n(r) = n1 · sqrt(1 - 2·delta·(r/a)^alpha)
+   * @param num_divisions 
+   * @param delta 
+   * @param n1 
+   * @param alpha 
+   * @param amplada 
+   * @param r 
+   */
+  compute_index_of_refraction(num_divisions: number, delta: number, n1: number, alpha: number, total_radius: number, r: number): number {
+    let inside_sqrt = 1 - 2*delta*Math.pow(r/total_radius, alpha);
+    return n1*Math.sqrt(inside_sqrt);
+  }
+
+  compute_indices_of_refraction_array(num_regions: number, delta: number, n1: number, alpha: number, total_radius: number): number[] {
+    let ns = [];
+    for (let i = 0; i < num_regions; ++i) {
+        let r = total_radius*(i/num_regions);
+        let n = this.compute_index_of_refraction(num_regions, delta, n1, alpha, total_radius, r);
+        ns.push(n);
+    }
+    return ns;
+  }
+
+  
 
   redraw() {}
 }
