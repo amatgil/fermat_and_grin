24896ed67038884e00d7799e9a89ca4cd03a2e83
Gerard Félez Guerrero <gerard.felez@estudiantat.upc.edu>
2026-05-21 18:58:25 +0200

    Starts working on the computation of the ray trajectory

--- grin/src/index.ts
+++ grin/src/index.ts
@@ -1,6 +1,10 @@
 import { Vec2 } from "./Vec2.js";
 import { init_listeners } from "./eventListeners.js";
 
+function degToRad(deg: number): number {
+    return (deg/360)*2*Math.PI;
+}
+
 export class ExperimentGrin {
   g: CanvasRenderingContext2D;
   g_width: number;
@@ -9,11 +13,12 @@ export class ExperimentGrin {
   delta: number;
   num_regions: number;
   
-  angle_raig: number;
   radi_fibra: number;
+  angle_raig: number;
+  height_raig: number;
   alpha: number;
 
-  longitud_cable: number;
+  cable_length: number;
   ns: number[];
 
 
@@ -26,10 +31,12 @@ export class ExperimentGrin {
     this.n1 = 1.47;
     this.delta = 0.01;
     this.num_regions = 10;
-    this.angle_raig = 1; // En radians
     this.radi_fibra = 1; // Ni idea de quines unitats posar lmao
+
+    this.angle_raig = degToRad(10); // En radians
+    this.height_raig = this.radi_fibra/2;
     this.alpha = 1;
-    this.longitud_cable = 100; // En metres (suposo)
+    this.cable_length = 100; // En metres (suposo)
     this.ns = [];
   }
 
@@ -43,22 +50,46 @@ export class ExperimentGrin {
    * @param amplada 
    * @param r 
    */
-  compute_index_of_refraction(num_divisions: number, delta: number, n1: number, alpha: number, total_radius: number, r: number): number {
+  static compute_index_of_refraction(num_divisions: number, delta: number, n1: number, alpha: number, total_radius: number, r: number): number {
     let inside_sqrt = 1 - 2*delta*Math.pow(r/total_radius, alpha);
     return n1*Math.sqrt(inside_sqrt);
   }
 
-  compute_indices_of_refraction_array(num_regions: number, delta: number, n1: number, alpha: number, total_radius: number): number[] {
-    let ns = [];
-    for (let i = 0; i < num_regions; ++i) {
-        let r = total_radius*(i/num_regions);
-        let n = this.compute_index_of_refraction(num_regions, delta, n1, alpha, total_radius, r);
-        ns.push(n);
+  compute_indices_of_refraction_array() {
+    this.ns = [];
+    for (let i = 0; i < this.num_regions; ++i) {
+        let r = this.radi_fibra*(i/this.num_regions);
+        let n = ExperimentGrin.compute_index_of_refraction(this.num_regions, this.delta, this.n1, this.alpha, this.radi_fibra, r);
+        this.ns.push(n);
     }
-    return ns;
   }
 
-  
+  // Aplies snell's law to compute the refracted angle
+  compute_refracted_angle(n1: number, theta_in: number, n2:number): number {
+    let sintheta = n1*Math.sin(theta_in)/n2;
+    return Math.asin(sintheta);
+  }
+
+  compute_ray_points() {
+    
+    let initial_y = this.height_raig;
+    let n0 = 1.0 // El raig comença a l'aire
+    let n1 = this.ns[0];
+    let theta_in = this.angle_raig;
+    // Angle with respect to the horizontal
+    let theta_out = this.compute_refracted_angle(n0, theta_in, n1); 
+    
+
+    let current_region = 0;
+    let current_point = new Vec2(0, this.height_raig);
+    let region_height = (2*this.num_regions-1)/(2*this.radi_fibra);
+    /*
+    while (current_point.x < this.cable_length) {
+        // TODO!!!
+    }*/
+  }
+
+
 
   redraw() {}
 }
